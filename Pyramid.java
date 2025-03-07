f4d5c3c2b6489b057a686620b8952217e67f0958
Aaditya Ramrame <161333408+Aadityaramrame@users.noreply.github.com>
2025-03-07 09:46:33 +0530

    Create Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,11 @@
+class Circle extends Shape {
+public class Pyramid {
+    super(length,0,0,0);
+    }
+    public double calculateArea(){
+        return  (dim_one * dim_one*dim_one);
+    }
+    public double calculatePerimeter(){
+        return 12*(dim_one);
+}
+}
