5a7a69b7de73651fa4359d542bbf29de19ea7fdb
Aaditya Ramrame <161333408+Aadityaramrame@users.noreply.github.com>
2025-03-08 19:27:58 +0530

    Create Sphere.java
    FInding the Area , Perimeter and Volume of the Sphere.

---
+++ Sphere.java
@@ -0,0 +1,14 @@
+class Sphere extends Shape implements Volume{
+    public Sphere(double radius){
+    super(radius,0,0,0);
+    }
+    public double calculateArea(){
+    return (4 * 3.14 * dim_one * dim_one);
+    }
+    public double calculatePerimeter(){
+    return 0;
+    }
+    public double calculateVolume(){
+    return 3.14 * dim_one * dim_one *dim_two;
+    }
+}
