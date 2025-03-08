57e493d9d984216338be67d4d4c4d87eaf24ccbf
Aaditya Ramrame <161333408+Aadityaramrame@users.noreply.github.com>
2025-03-08 19:26:56 +0530

    Create Cylinder.java
    Finding the Cylinder Area, perimeter and volume

---
+++ Cylinder.java
@@ -0,0 +1,14 @@
+class Cylinder extends Shape implements Volume {
+    public Cylinder(double radius, double height){
+    super(radius, height, 0, 0);
+    }
+    public double calculateArea(){
+    return 2 * 3.14 * dim_one * (dim_one + dim_two);
+    }
+    public double calculatePerimeter(){
+    return 2 * 3.14 * dim_one;
+    }
+    public double calculateVolume(){
+    return 3.14 * dim_one * dim_one * dim_two;
+    }
+}
