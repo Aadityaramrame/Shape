594b80228561cca942d7cbbc2860f12845bdad22
Aaditya Ramrame <161333408+Aadityaramrame@users.noreply.github.com>
2025-03-08 19:25:09 +0530

    Create Square.java
    Uploading the Square Class

---
+++ Square.java
@@ -0,0 +1,11 @@
+class Square extends Shape{
+    public Square(double side){
+    super(side,side,0,4);
+    }
+    public double calculateArea(){
+    return (dim_one * dim_two);
+    }
+    public double calculatePerimeter(){
+    return 4 * dim_one;
+    }
+}
